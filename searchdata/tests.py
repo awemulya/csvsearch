35fe50eeda69d2ad216b031a20cdab21aa3e7b73
awemulya <aweamulya@gmail.com>
2015-09-06 14:31:56 +0545

    test for sample record

--- searchdata/tests.py
+++ searchdata/tests.py
@@ -1,3 +1,23 @@
+import os
 from django.test import TestCase
+from searchdata.utils import search
 
-# Create your tests here.
+
+class SearchTestCase(TestCase):
+    def setUp(self):
+        self.path = os.path.join(os.getcwd(), 'testfile.csv')
+        self.first_name = "James"
+        self.first_name_bad = "James333"
+        self.last_name = "Butt"
+        self.phone = "504-621-8927"
+        self.address = "6649 N Blue Gum St"
+
+    def test_data_found(self):
+        """Data found according to search"""
+        status = search(self.path, self.first_name, self.last_name, self.phone, self.address)
+        assert status
+
+    def test_data_not_found(self):
+        """Data found according to search"""
+        status = search(self.path, self.first_name_bad, self.last_name, self.phone, self.address)
+        assert not status
