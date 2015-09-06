535484937ef79ed8d5037d965ed2a6e64d19e2d0
awemulya <aweamulya@gmail.com>
2015-09-06 13:03:19 +0545

    search data app listed in settings

--- csvsearch/settings.py
+++ csvsearch/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'searchdata',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'csvsearch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['searchdata','searchdata/templates'],
+        'DIRS': ['searchdata'],
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
